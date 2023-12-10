a07be8aa2cb210979dc9a41dc3eb07433ff152bf
Dilshodbek <dilshodj544@gmail.com>
2023-12-10 20:11:01 +0500

    add lorem

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(props) {
     return (
         <div className='bgColor'>
-            hello world
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo atque nostrum officiis ducimus harum eos pariatur doloremque! Harum corporis dolor accusantium officia officiis iste sequi earum dolorem, quo quisquam soluta.
         </div>
     );
 }
