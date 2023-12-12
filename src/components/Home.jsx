b4a5bcb5dc07f652e9154fe2e410a29e7d1fb870
Dilshodbek <dilshodj544@gmail.com>
2023-12-12 11:35:36 +0500

    new background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,8 @@ import Loader from './utils/Loader';
 
 function Home(props) {
     return (
-<<<<<<< HEAD
-        <div className='bgColor'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo atque nostrum officiis ducimus harum eos pariatur doloremque! Harum corporis dolor accusantium officia officiis iste sequi earum dolorem, quo quisquam soluta.3
-=======
         <div>
             <Loader />
->>>>>>> 17b0d2a6c728dc6f523ceaa28037ce5034c75185
         </div>
     );
 }
