1c6211d1682b2080e49d04556d27460cfea45021
azizahadovv <aziz.ahadovv@yandex.ru>
2023-12-12 11:35:37 +0500

    new loader

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Loader from './utils/Loader';
+import { LOADER } from './exporPage';
 
 function Home(props) {
     return (
-<<<<<<< HEAD
-        <div className='bgColor'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo atque nostrum officiis ducimus harum eos pariatur doloremque! Harum corporis dolor accusantium officia officiis iste sequi earum dolorem, quo quisquam soluta.3
-=======
         <div>
-            <Loader />
->>>>>>> 17b0d2a6c728dc6f523ceaa28037ce5034c75185
+            <LOADER />
         </div>
     );
 }
