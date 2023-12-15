d4b396da8cbd7bbb32405487e790d96164fc1fe7
azizahadovv <aziz.ahadovv@yandex.ru>
2023-12-15 15:06:44 +0500

    Clear Auth Add Navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar(props) {
         <div className='w-full min-h-[70px] bg-[rgba(0,0,0,.5)]  fixed '>
             <nav className='flex items-center justify-between minMobil:w-full tablet:w-[70%] m-auto  h-[70px]'>
                 <img src={{}} alt="#" />
-                <button className='px-3 py-2 bg-[rgba(147,82,224,.5)] text-white rounded-lg text-xl'>Call Me</button>
+                <button className='px-3 py-2 bg-[rgba(0,0,0,.5)] border  hover:bg-[rgba(255,255,255)]  text-white hover:text-black rounded-lg text-xl'>Call Me</button>
             </nav>
         </div>
     );
