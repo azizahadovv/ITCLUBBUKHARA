fd03c687bd706c43bba7406a76091a2c00624440
azizahadovv <aziz.ahadovv@yandex.ru>
2023-12-17 12:41:43 +0500

    Navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ setModalVisible, modalVisible }) {
             <nav className='flex items-center justify-between minMobil:w-full tablet:max-w-[900px] m-auto  h-[70px]'>
                 <div className='flex items-center justify-center h-full align-baseline '>
                     <img className='w-20 h-full -rotate-[5deg] flex items-center justify-center' src={LOGO} alt="#" />
-                    <span className='flex items-center justify-center text-xl text-white'>IT.CLUB BUKHARA</span>
+                    <span className='flex items-center justify-center text-xl text-white'>IT.git CLUB BUKHARA</span>
                 </div>
                 <button onClick={() => (setModalVisible(true))} className='tablet:px-3 tablet:py-2 minMobil:px-3 minMobil:py-2 tablet:text-white minMobil:text-white bg-[rgba(0,0,0,.5)] border  hover:bg-[rgba(255,255,255)]   hover:text-black rounded-lg teblet:text-xl minMobil:text-base'>Call Me</button>
             </nav>
