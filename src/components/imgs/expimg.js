d32eda8d49acbf96e7fad4abd24658f9b9c495b3
Dilshodbek <dilshodj544@gmail.com>
2023-12-17 18:22:51 +0500

    Add Courses

--- src/components/imgs/expimg.js
+++ src/components/imgs/expimg.js
@@ -1,2 +1,5 @@
 export { default as Error } from "./error.svg";
 export { default as LOGO } from "./imglogo.svg";
+export { default as WEB } from "./web_Development.png";
+export { default as BESIC} from "./besic_operator.png"
+export { default as MOBILE} from "./Mobile.png"
--- src/components/pages/Introduction.jsx
+++ src/components/pages/Introduction.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Introduction(props) {
     return (
-        <div className='w-full h-[340px] bg-[rgba(0,0,0,.4)] brightness-95   flex items-center justify-center border-t border-b border-[rgba(156,152,152,.7)]'>
+        <div className='w-full h-[340px] bg-[rgba(0,0,0,.4)] brightness-95 flex items-center justify-center border-t border-b border-[rgba(156,152,152,.7)]'>
             <div className='text-center'>
                 <span className='text-4xl text-white'>Introduction</span>
                 <p className='text-xl text-white mt-7'>Programming is an essential skill in today's digital world. Our Programming Learning Center provides the <br /> perfect platform for learners of all levels to master programming concepts and technologies. With our <br /> courses and experienced instructors, we make learning both fun and effective.</p>
