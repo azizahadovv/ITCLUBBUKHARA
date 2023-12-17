17c83435597dd70286b11c4ac9d34caecb9694a6
azizahadovv <aziz.ahadovv@yandex.ru>
2023-12-17 14:01:32 +0500

    bg-blur, filter site, add intro, add to home folder

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,9 +1,10 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase/firebase';
 import { LOADER } from '../exporPage';
 import { MdDelete } from "react-icons/md";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function Admin(props) {
     const [dbMap, setDbMap] = useState([])
     const [load, setLoad] = useState(false)
@@ -27,18 +28,25 @@ function Admin(props) {
         })
     }
 
-    function deleteUser(user) {
+    function deleteUser(id) {
+        setLoad(true)
+        const delConfig = doc(db, 'users', id)
+        deleteDoc(delConfig).then((res) => {
+            toast.success('SUCCESSFULL')
+            getUserDb()
+            setLoad(false)
+        }).catch((err) => { toast.error(err) })
 
     }
 
 
 
     return (
-        <div className='w-full min-h-[100vh] bg-[rgba(0,0,0,.3)] p-3 flex gap-3 justify-start items-start flex-wrap'>
+        <div className='w-full min-h-[100vh] bg-[rgba(0,0,0,.3)]  flex gap-3 justify-start items-start flex-wrap'>
             {
-                load ? <LOADER /> : dbMap.map((res) => {
+                load ? <LOADER /> : dbMap.length === 0 ? <h1 className='text-white '>Hozircha hech qanaqa ma'lumot topilmadi!?</h1> : dbMap.map((res) => {
                     return <div className="mb-3 card text-bg-secondary w-[250px] relative" >
-                        <div class="card-header"><span className='text-green-500'>Name:</span> {res?.fullName} <button onClick={() => (deleteUser(res))} className='absolute -top-2 -right-3 btn btn-danger'><MdDelete /></button></div>
+                        <div class="card-header"><span className='text-green-500'>Name:</span> {res?.fullName} <button onClick={() => (deleteUser(res.id))} className='absolute -top-2 -right-3 btn btn-danger'><MdDelete /></button></div>
                         <div class="card-body">
                             <h5 class="card-title"><span className='text-yellow-400'>Tell:</span> {res?.Phone}</h5>
                             <p class="card-text flex flex-wrap gap-1"><span className='text-red-400'>Email: </span> {res?.email}</p>
--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { LOGO } from '../imgs/expimg';
+import { Link } from 'react-router-dom';
 
 function Navbar({ setModalVisible, modalVisible }) {
     return (
-        <div className='w-full min-h-[70px] bg-[rgba(0,0,0,.5)] fixed px-5'>
+        <div className='w-full min-h-[70px] bg-[rgba(0,0,0,.8)] fixed px-5'>
             <nav className='flex items-center justify-between minMobil:w-full tablet:max-w-[900px] m-auto  h-[70px]'>
-                <div className='flex items-center justify-center h-full align-baseline '>
+                <Link to={'/'} className='flex items-center justify-center h-full overflow-hidden no-underline align-baseline'>
                     <img className='w-20 h-full -rotate-[5deg] flex items-center justify-center' src={LOGO} alt="#" />
-                    <span className='flex items-center justify-center text-xl text-white'>IT.git CLUB BUKHARA</span>
-                </div>
+                    <span className='flex items-center justify-center text-xl text-white'>IT.CLUB BUKHARA</span>
+                </Link>
                 <button onClick={() => (setModalVisible(true))} className='tablet:px-3 tablet:py-2 minMobil:px-3 minMobil:py-2 tablet:text-white minMobil:text-white bg-[rgba(0,0,0,.5)] border  hover:bg-[rgba(255,255,255)]   hover:text-black rounded-lg teblet:text-xl minMobil:text-base'>Call Me</button>
             </nav>
         </div>
